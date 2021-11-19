c384a06364f240cbe637f12ab3e2c562c0905840
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-19 14:08:01 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Products from './components/Projects/Products';
+import {Products, Navbar} from './components';
 
 
 const App = () =>  {
   return (
     <div>
+      <Navbar/>
       <Products/>
     </div>
   );
