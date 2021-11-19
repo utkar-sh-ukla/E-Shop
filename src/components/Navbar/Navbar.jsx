df97c83c807815722191048a7c1964fbd4e8d374
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-19 16:55:33 +0530

    Add to Cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
 
-const Navbar = () => {
+const Navbar = ({totalItems}) => {
   const classes = useStyles();
 
   return (
@@ -31,7 +31,7 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton color='inherit' arial-label='Show Cart Items'>
-              <Badge badgeContent={2} color='secondary'>
+              <Badge badgeContent={totalItems} color='secondary'>
                 <ShoppingCart />
               </Badge>
             </IconButton>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,7 +3,7 @@ import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles'
 
-function Products({products}) {
+function Products({products, onAddToCart}) {
     const classes = useStyles();
 
     return (
@@ -12,7 +12,7 @@ function Products({products}) {
         <Grid container justify='center' spacing={4}>
           {products.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <Product product={product}/>
+              <Product product={product} onAddToCart={onAddToCart}/>
             </Grid>
           ))}
         </Grid>
