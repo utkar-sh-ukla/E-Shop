ecdad801f95022bdabbf8db19be79afae5727126
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-26 13:56:38 +0530

    Checkout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
@@ -63,6 +63,9 @@ const App = () => {
               handleRemoveFromCart={handleRemoveFromCart}
               handleEmptyCart={handleEmptyCart}
               />
+          </Route>
+          <Route exact path='/checkout'>
+            <Checkout/>
           </Route>
         </Switch>
       </div>
