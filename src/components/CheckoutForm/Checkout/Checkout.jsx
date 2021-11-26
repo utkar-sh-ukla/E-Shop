800f9b4d3bfc26b1d426aef2bdb8030db8da6b95
Utkarsh Shukla <utkarshshukla707@gmail.com>
2021-11-27 01:22:26 +0530

    Review

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -24,7 +24,7 @@ const Checkout = ({cart}) => {
         const generateToken = async () => {
             try{
                 const token = await commerce.checkout.generateToken(cart.id, {type: 'cart'})
-                console.log(token)
+                // console.log(token)
                 setCheckoutToken(token)
             }catch(error){
                 // console.log(error)
@@ -45,7 +45,15 @@ const Checkout = ({cart}) => {
             </div>
         )
     }
-    const Form = () => activeStep === 0 ? <AddressForm checkoutToken={checkoutToken} next={next}/> : <PaymentForm shippingData={shippingData}/>
+    const Form = () =>
+      activeStep === 0 ? (
+        <AddressForm checkoutToken={checkoutToken} next={next} />
+      ) : (
+        <PaymentForm
+          shippingData={shippingData}
+          checkoutToken={checkoutToken}
+        />
+      );
     return (
         <>
             <div className={classes.toolbar}/>
--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Typography, Button, Divider } from '@material-ui/core'
+import {Elements, CardElement, ElementsConsumer} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
 
-const PaymentForm = () => {
-    return (
-        <div>
-            PaymentForm
-        </div>
-    )
-}
+import Review from './Review'
+
+const PaymentForm = ({ checkoutToken }) => {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
+};
 
 export default PaymentForm
